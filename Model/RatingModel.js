07f2826625e1fea247c9d45905d971a105ce70f5
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2025-01-17 10:07:51 +0530

    update

---
+++ Model/RatingModel.js
@@ -0,0 +1,33 @@
+// models/Rating.js
+const mongoose = require("mongoose");
+
+const ratingSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    review: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Rating = mongoose.model("Rating", ratingSchema);
+
+module.exports = Rating;
